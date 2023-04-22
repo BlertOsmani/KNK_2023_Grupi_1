26c00063bda8cdeb6039fea633e8f879e10c2730
BlertOsmani <blertosmani15@gmail.com>
2023-04-22 15:03:38 +0200

    KNK

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.knk_2023_grupi_1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.knk_2023_grupi_1 to javafx.fxml;
+    exports com.example.knk_2023_grupi_1;
+}
