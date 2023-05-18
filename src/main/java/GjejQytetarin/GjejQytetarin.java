dfba14d054efd0a5a18ac0d9dc9392d4fc63e136
Astrit Krasniqi <121978549+astritkrasniqi1@users.noreply.github.com>
2023-05-18 16:16:47 +0200

    GjejQytetarin

--- src/main/java/GjejQytetarin/GjejQytetarin.java
+++ src/main/java/GjejQytetarin/GjejQytetarin.java
@@ -1,4 +1,30 @@
 package GjejQytetarin;
 
-public class GjejQytetarin {
+
+import Adresa.Adresa;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static javafx.application.Application.launch;
+
+public class GjejQytetarin extends Application {
+    @Override
+    public void start(Stage stage) throws IOException, SQLException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GjejQytetarin.class.getResource("GjejQytetarin.fxml"));
+        Pane pane = fxmlLoader.load();
+        Scene scene = new Scene(pane, pane.getWidth()-100, pane.getHeight()-100);
+        stage.setTitle("Aplikacion per kerkimin e qytetarit");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public static void main(String[]args) throws SQLException {
+        launch();
+    }
 }
