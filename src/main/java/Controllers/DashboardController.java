36676404fcd7606f1acc5b6e5560e9d43fc876a4
Astrit Krasniqi <121978549+astritkrasniqi1@users.noreply.github.com>
2023-05-21 17:02:27 +0200

    dashboard

---
+++ src/main/java/Controllers/DashboardController.java
@@ -0,0 +1,27 @@
+package Controllers;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+
+public class DashboardController {
+    @FXML
+    private BarChart<String, Number> barChart;
+
+    public void initialize() {
+        // Create data for the chart
+        ObservableList<XYChart.Series<String, Number>> barChartData = FXCollections.observableArrayList();
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>("Category 1", 10));
+        series.getData().add(new XYChart.Data<>("Category 2", 20));
+        series.getData().add(new XYChart.Data<>("Category 3", 15));
+        barChartData.add(series);
+
+        // Set the data to the Bar Chart
+        barChart.setData(barChartData);
+    }
+
+}
